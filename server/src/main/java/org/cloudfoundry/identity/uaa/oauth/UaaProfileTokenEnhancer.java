90c112ee57f226780a86d857e07c9ed37bae7c7c
Castilho, N <nuno_castilho78@hotmail.com>
2020-11-24 00:58:33 +0000

    PUR-66 UAA debug profile.sub fix

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/UaaProfileTokenEnhancer.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/UaaProfileTokenEnhancer.java
@@ -1,9 +1,11 @@
 package org.cloudfoundry.identity.uaa.oauth;
 
 import org.cloudfoundry.identity.uaa.authentication.UaaAuthentication;
+import org.cloudfoundry.identity.uaa.authentication.UaaPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,30 +16,24 @@ public class UaaProfileTokenEnhancer implements UaaTokenEnhancer {
 
     @Override
     public Map<String, String> getExternalAttributes(OAuth2Authentication authentication) {
-        System.out.println("authentication = " + authentication);
-
-//        UaaAuthentication oldAuthentication = (UaaAuthentication) SecurityContextHolder.getContext().getAuthentication();
-//
-//        System.out.println(oldAuthentication);
-//        System.out.println(oldAuthentication.getPrincipal());
-//        System.out.println(oldAuthentication.getPrincipal().getId());
-
         Map<String, String> externalAttributes = new HashMap<>();
 
-//        externalAttributes.put(SUB, oldAuthentication.getPrincipal().getId());
+        externalAttributes.put(SUB, ((UaaPrincipal)authentication.getPrincipal()).getId());
 
         return externalAttributes;
     }
 
     @Override
     public Map<String, Object> enhance(Map<String, Object> claims, OAuth2Authentication authentication) {
-        System.out.println("claims = " + claims + ", authentication = " + authentication);
+        if (!(authentication.getPrincipal() instanceof UaaPrincipal)) {
+            return Collections.emptyMap();
+        }
 
         Map<String, Object> profile = new HashMap<>();
 
-//        profile.put(PROFILE, getExternalAttributes(authentication));
-//
-//        System.out.println("Returning " + profile);
+        profile.put(PROFILE, getExternalAttributes(authentication));
+
+        System.out.println("Returning " + profile);
 
         return profile;
     }
